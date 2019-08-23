77e3abf3462f9d0e8be484ffa75109efbd9aa066
snsaleh1 <snsaleh11@gmail.com>
2019-08-22 22:11:09 -0500

    uncommented functions

--- controllers/yelp.js
+++ controllers/yelp.js
@@ -8,30 +8,30 @@ module.exports = {
 
 function yelpSearch(req, res){
   console.log(req.body.where);
-  //   if(req.body.what === "breakfast" || req.body.what === "lunch" || req.body.what === "dinner"){
-  //   client.search({
-  //     term: 'Restaurant',
-  //     //model will be dropdown? I prefer input..(req.body.where({}))
-  //     location: (req.body.where),
-  //     limit: 5
-  //   }).then(response => {
-  //     console.log(response.jsonBody.businesses);
-  //     let result = response.jsonBody.businesses
-  //     res.status(200).json(result)
-  //   }).catch(e => {
-  //     console.log(e);
-  //   });
-  // } else if(req.body.what === "coffee"){
-  //   client.search({
-  //       term: 'coffee shop',
-  //       location: 'austin',
-  //       limit: 5
-  //     }).then(response => {
-  //       console.log(response.jsonBody.businesses);
-  //       let result = response.jsonBody.businesses
-  //       res.status(200).json(result)
-  //     }).catch(e => {
-  //       console.log(e);
-  //     });
-  // }
+    if(req.body.what === "breakfast" || req.body.what === "lunch" || req.body.what === "dinner"){
+    client.search({
+      term: 'Restaurant',
+      //model will be dropdown? I prefer input..(req.body.where({}))
+      location: (req.body.where),
+      limit: 5
+    }).then(response => {
+      console.log(response.jsonBody.businesses);
+      let result = response.jsonBody.businesses
+      res.status(200).json(result)
+    }).catch(e => {
+      console.log(e);
+    });
+  } else if(req.body.what === "coffee"){
+    client.search({
+        term: 'coffee shop',
+        location: 'austin',
+        limit: 5
+      }).then(response => {
+        console.log(response.jsonBody.businesses);
+        let result = response.jsonBody.businesses
+        res.status(200).json(result)
+      }).catch(e => {
+        console.log(e);
+      });
+  }
 };
